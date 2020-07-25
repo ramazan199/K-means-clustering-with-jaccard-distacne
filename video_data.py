2bad6f74680ad6e28eb840af3b4e77b0b3595b12
ramazan199 <ramazansuleymanli@gmail.com>
2020-07-25 17:10:20 +0300

    seperate file for vides

---
+++ video_data.py
@@ -0,0 +1,427 @@
+VideosTags1 = '''Lose Yourself (Musical Recording)
+Hip Hop Music (Musical Genre)
+Eminem (Music Video Performer)
+Rap Rock (Musical Genre)
+Music (TV Genre)
+8 Mile (Film)
+8 Mile (Soundtrack)
+The Slim Shady LP (Musical Album)
+official
+Trailer (Website Category)
+MTV (TV Network)
+Music Video (TV Genre)
+libra
+The Marshall Mathers LP (Musical Album)'''
+
+
+VideosTags2 ='''Eminem
+Not
+Afraid
+Aftermath
+Hip
+Hop
+New Song
+Recovery
+Relapse
+Em
+eminem not afraid
+eminem vevo'''
+
+VideosTags3 ='''twenty one pilots
+blurryface
+stressed out
+21 pilots
+twentyonepilots
+fueled by ramen
+FBR
+fueledbyramen
+tyler joseph
+josh dun
+TOP
+top
+top stressed out
+21 pilots stressed out
+stressed out blurryface
+stressed out video
+stressed out official video
+stressed out fueled by ramen
+stressed out FBR
+wish we could turn back time
+my name's Blurryface
+top stressedout
+but now we're stressed out
+blurryface stressed out
+wake up you need to make money
+stress out'''
+
+
+VideosTags4 = '''twenty one pilots
+blurryface
+ride
+blurry face
+Fueled By Ramen
+FBR
+21 pilots
+twentyone pilots
+fueledbyramen
+tyler joseph
+josh dun
+official video
+music video
+twenty one pilots ride
+ride twenty one pilots
+top ride ride top
+skeleton clique
+Im falling so Im taking my time on my ride
+oh Im falling so Im taking my time on my ride
+Ive been thinking too much
+Ive been thinking too much help me
+twenty one pilots ride official video
+top ride official'''
+
+VideosTags5 = '''cats
+cute
+meow
+kitty
+cat videos
+'''
+VideosTags6 = '''cat
+kitten
+cute
+kitty
+cat videos
+'''
+VideosTags7 = '''6ix9ine
+gooba
+tekashi
+tekashi 69
+music video
+gooba 6ix9ine
+6ix9ine gooba
+69 gooba
+gooba 69
+tekashi 6ix9ine gooba
+hip hop
+rap
+king of new york
+6ix9ine live
+new 6ix9ine
+create music group
+10k projects
+bebe
+gummo
+tati
+stoopid
+nicki minaj
+new york
+king of ny
+ig live
+rainbow
+goba
+six nine
+fefe
+anuel aa
+murda beatz
+jah
+tik tok songs
+'''
+VideosTags8 = '''
+future
+drake
+hip hop
+travis scott
+doe boy
+meek mill
+young thug
+hndrxx
+migos
+codeine crazy
+the wizrd
+freebandz
+freeband gang
+fbg
+life is good
+epic
+new music
+rap
+Epic/Freebandz
+Future feat. Drake
+Life Is Good
+Rap
+'''
+VideosTags9 = '''snowrunner
+snowrunner gameplay
+snowrunner gameplay part 1
+snowrunner part 1
+snowrunner walkthrough part 1
+snowrunner best heavy truck
+'''
+VideosTags10 = '''snowrunner
+snowrunner gameplay
+snowrunner playtrough
+snowrunner heavy duty cars
+snowrunner walktrough
+snowrunner trailer
+SnowRunner Top Twitch Funny Moments Compilation
+SnowRunner funny moments
+SnowRunner compilation
+SnowRunner trucking simulator
+SnowRunner best simulator
+games like SnowRunner
+mudrunner
+Spintires: MudRunner
+MudRunner 2
+'''
+#Coolio - Gangsta's Paradise (feat. L.V.) [Official Music Video]
+
+VideosTags11 = '''Coolio
+Gangsta Rap
+Cali Rap
+Lengendary Hip Hop
+L.v.
+Gangsters Paradises
+Gangstas Paradise
+Tommy Boy Records
+90S
+West Coast Rap
+Tommy Boy
+Tommy Boy Music
+POWER AND THE MONEY
+MONEY AND THE POWER.
+Livin' in a gangsta's paradise
+Doug Rasheed
+Stevie Wonder
+TBMClassics
+Armish paradise
+Gangsta's Paradise
+Tommy Boy Entertainment
+My Soul
+It Takes a Thief
+Golden Era
+Hip Hop
+Rap
+C U When U Get There
+Sumpin New
+Michelle Pfeiffer
+Dangerous Minds
+Werid Al
+'''
+
+#Gorillaz - Clint Eastwood (Official Video)
+
+VideosTags12 = '''gorillaz
+Clint Eastwood
+clint
+eastwood
+official
+video
+storyboard
+feel
+good
+inc
+alternative
+indie
+pop
+parlophone
+music
+'''
+
+#Gorillaz - Feel Good Inc. (Official Video)
+
+VideosTags13 = '''gorillaz
+Feel Good Inc.
+Feel Good Inc
+feel
+good
+inc
+official
+video
+alternative
+indie
+pop
+parlophone
+'''
+
+#Gorillaz - On Melancholy Hill (Official Video)
+
+VideosTags14 = '''Gorillaz
+On
+Melancholy
+Hill
+Parlophone
+On Melancholy Hill
+Plastic Beach
+Plastic
+Beach
+Trip Hop
+Rock
+'''
+
+#2pac feat Dr.Dre - California Love HD
+
+VideosTags15 = '''2Pac
+dr.dre
+dr
+dre
+tupac
+1994
+1995
+death
+row
+california
+love
+HD
+west
+side
+westcoast
+coast
+rap
+hip
+hop
+'''
+
+#2Pac - Hit 'Em Up (Dirty) (Official Video) HD
+
+VideosTags16 = '''Real
+Rap
+Won't
+Die
+Rapping (Profession)
+Tupac Shakur (Rapper)
+Music Video (Product Line)
+Video Clip (Website Category)
+2pac
+pac
+tu
+two
+hit
+'em
+em
+them
+up
+'''
+
+#Doja Cat - "Mooo!" (Official Video)
+
+VideosTags17 = '''doja cat
+moo
+doja cat moo
+bitch I'm a cow
+Amala
+Go To Town
+'''
+
+#CS:GO's Grenade Blast Update
+
+VideosTags18 = '''counter-strike global offensive
+gameplay
+counter-strike
+CS:GO
+cs
+cs go
+csgo server
+3kliksphilip
+gameplay pc hd
+pc gaming
+cs go 2020
+'''
+
+#Making a Jacob's Ladder to Celebrate a Million Subs!
+
+VideosTags19 = '''educational
+electrical
+electroboom
+electronics
+engineering
+entertainment
+equipment
+mehdi
+mehdi sadaghdar
+arc
+mishap
+physics
+sadaghdar
+science
+test
+tools
+circuit
+funny
+learn
+shock
+spark
+protection
+short circuit
+microwave
+oven
+transformer
+high voltage
+high power
+3D printer
+lulzbot
+lulzbot mini
+Jacob
+jacobs ladder amplifier
+multiplier
+AC and DC
+'''
+
+#How NOT to Make an Electric Guitar (The Hazards of Electricity)
+
+VideosTags20 = '''tech
+technology
+electronic
+electronics
+electrical
+guitars
+electric guitars
+electric guitar review
+f-hole electric guitar
+hole electric guitar
+type electric guitar
+electric guitar phenomena
+msadaghd
+mehdi sadaghdar
+tutorial
+guitar
+electric guitar
+electricity
+shock
+educational
+electroboom
+engineering
+entertainment
+equipment
+mehdi
+mishap
+physics
+sadaghdar
+science
+test
+tools
+circuit
+funny
+learn
+'''
+
+
+# VideosTags1 = '''a
+# b
+# c
+# d'''
+
+# VideosTags2 = '''a
+# b
+# c
+# e'''
+# VideosTags3 = '''a
+# w
+# c
+# e'''
+
+
+# VideosTags4 = '''g
+# h
+# j'''
+# VideosTags5 = '''g
+# h
+# y'''
